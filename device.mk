0582e48389f51ca33ace499f49e2460c98bd7895
TheStrix <parthbhatia@lineageos.org>
2017-06-24 13:00:12 +0200

    Get some things from sagit tree
    Change-Id: Ic828cf4237df1a3318e633950e37ab483d07abda

--- device.mk
+++ device.mk
@@ -103,6 +103,7 @@ PRODUCT_PACKAGES += \
 ifeq ($(RECOVERY_VARIANT),twrp)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:recovery/root/init.recovery.usb.rc \
     $(LOCAL_PATH)/recovery/root/twrp.fstab:recovery/root/etc/twrp.fstab
 endif
 
