315e05b6935c4f87ab9a963f3f13dc2ef1328085
XiNGRZ <chenxingyu92@gmail.com>
2017-06-28 01:40:24 +0800

    op5: start with empty files
    Change-Id: Ic44df6ff85656b1cec4a29012b97b2529cba075f

---
+++ extract-files.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The MoKee Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=cheeseburger
+VENDOR=oneplus
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+MK_ROOT="$MY_DIR"/../../..
+
+HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT"
+
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
+extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC"
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
+
+"$MY_DIR"
---
+++ setup-makefiles.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The MoKee Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=cheeseburger
+VENDOR=oneplus
+
+INITIAL_COPYRIGHT_YEAR=2017
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+MK_ROOT="$MY_DIR"/../../..
+
+HELPER="$MK_ROOT"/vendor/mk/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$MK_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# Qualcomm BSP blobs - we put a conditional around here
+# in case the BSP is actually being built
+printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$PRODUCTMK"
+printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$ANDROIDMK"
+
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt
+
+# Qualcomm performance blobs - conditional as well
+# in order to support Cyanogen OS builds
+cat << EOF >> "$PRODUCTMK"
+endif
+
+-include vendor/extra/devices.mk
+ifneq (\$(call is-qc-perf-target),true)
+EOF
+
+cat << EOF >> "$ANDROIDMK"
+endif
+
+ifneq (\$(TARGET_HAVE_QC_PERF),true)
+EOF
+
+write_makefiles "$MY_DIR"/proprietary-files-qc-perf.txt
+
+echo "endif" >> "$PRODUCTMK"
+
+cat << EOF >> "$ANDROIDMK"
+
+endif
+
+\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
+\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
+EOF
+
+# We are done!
+write_footers
