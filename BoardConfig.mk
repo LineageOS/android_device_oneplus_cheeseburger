c665f75fb74ee75add6956e6e7e1c683ff4fddd4
XiNGRZ <chenxingyu92@gmail.com>
2017-07-03 13:08:32 +0800

    op5: double tap to wake
    Change-Id: Ida9ef14200e7d8239ee71589c70e94662a0b67d9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # MK Hardware
 # BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/mkhw
-# TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
 BOARD_USES_QCNE := true
