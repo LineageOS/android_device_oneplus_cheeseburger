354769c41785638fe6a64e155f7d44cd7669369b
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-06 09:56:33 +0200

    cheeseburger: add missing aanc_tuning_mixer config
    Change-Id: Iaf0323f52d80f24dcf8987fa14059df919a5b288

--- device.mk
+++ device.mk
@@ -105,6 +105,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
