95a9ebcb72ff186466951b9fe53eb4c00b7585d8
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-24 20:55:55 -0700

    cheeseburger: add netutils-wrapper impl
    Change-Id: Ida6feaf17b054079a598931d9e15a86c7e5bf843

--- device.mk
+++ device.mk
@@ -263,6 +263,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
