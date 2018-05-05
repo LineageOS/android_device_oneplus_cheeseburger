bb5eca715f88e0323dff6540c9cee0788082a2d5
Alexander Martinz <alex@amartinz.at>
2018-05-05 18:34:45 +0200

    cheeseburger: commonize config.fs
    Change-Id: I1df48399211703e344f3e2e1084f800fbb68f265
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,9 +34,6 @@ TARGET_OTA_ASSERT_DEVICE := cheeseburger,OnePlus5,oneplus5
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Filesystem
-TARGET_FS_CONFIG_GEN += $(DEVICE_PATH)/config.fs
-
 # Lineage Hardware
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
 
