354b3102adacda37faad40aecf776d51edbc5180
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    cheeseburger: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ TARGET_OTA_ASSERT_DEVICE := cheeseburger,OnePlus5,oneplus5
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # SELinux
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
