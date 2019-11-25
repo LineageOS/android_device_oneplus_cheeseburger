675e02adaa0b823602f71dab0d29f73eac361db7
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-11-25 08:22:03 +0100

    cheeseburger: selinux: add policy for touch hal
    Change-Id: Ia66342c356d29f6c086fa81fc4fe9782dad197cf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,11 @@ TARGET_OTA_ASSERT_DEVICE := cheeseburger,OnePlus5,oneplus5
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# SELinux
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+
 # inherit from the proprietary version
 -include vendor/oneplus/cheeseburger/BoardConfigVendor.mk
 
