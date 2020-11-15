ab44ed38a279091930f1eb8ad6dc3babca898509
Georg Veichtlbauer <georg@vware.at>
2020-11-15 10:10:18 +0100

    cheeseburger: move keylayout from common repo
    Change-Id: I2d6705e3f0af609c20b25ed58ae9a43a0de00fb0

--- device.mk
+++ device.mk
@@ -44,5 +44,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.cheeseburger
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gf_input.kl:system/usr/keylayout/gf_input.kl
+
 # Inherit from oneplus msm8998-common
 $(call inherit-product, device/oneplus/msm8998-common/common.mk)
