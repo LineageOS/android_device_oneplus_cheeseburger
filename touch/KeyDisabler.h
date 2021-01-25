620bd059410fafd249db7cd514d6ec6d929ba750
dianlujitao <dianlujitao@lineageos.org>
2021-01-25 16:20:39 +0800

    msm8998-common: touch: Implement key swapper interface
    Apply minor clean-ups while at it
    
    Change-Id: I007624124997b46a0ed2961b938cd488efad45f2

--- touch/KeyDisabler.h
+++ touch/KeyDisabler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_LINEAGE_TOUCH_V1_0_KEYDISABLER_H
-#define VENDOR_LINEAGE_TOUCH_V1_0_KEYDISABLER_H
+#pragma once
 
 #include <vendor/lineage/touch/1.0/IKeyDisabler.h>
 
@@ -37,7 +36,7 @@ class KeyDisabler : public IKeyDisabler {
     Return<bool> setEnabled(bool enabled) override;
 
   private:
-    bool mHasKeyDisabler;
+    const bool has_key_disabler_;
 };
 
 }  // namespace implementation
@@ -45,5 +44,3 @@ class KeyDisabler : public IKeyDisabler {
 }  // namespace touch
 }  // namespace lineage
 }  // namespace vendor
-
-#endif  // VENDOR_LINEAGE_TOUCH_V1_0_KEYDISABLER_H
