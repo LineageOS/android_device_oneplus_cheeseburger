4f02dace1f8cb62c2f0c46de69a71f1af8b87714
Georg Veichtlbauer <georg@vware.at>
2021-09-06 17:39:58 +0200

    cheeseburger: Import device specific keylayouts
    * From msm8998-common
    * Also pull sepolicy over
    
    Change-Id: I880d87e070fa78326fc4b84b493e141c9c3be7ce

--- device.mk
+++ device.mk
@@ -45,7 +45,9 @@ PRODUCT_COPY_FILES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gf_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gf_input.kl
+    $(LOCAL_PATH)/keylayout/fpc1020.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/fpc1020.kl \
+    $(LOCAL_PATH)/keylayout/gf_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gf_input.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_s3320.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_s3320.kl
 
 # Lineage hardware
 PRODUCT_PACKAGES += \
