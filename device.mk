6437fa90f163d398ad5e9092859a5b5dfa42e00a
Georg Veichtlbauer <georg@vware.at>
2022-07-23 00:34:47 +0200

    cheeseburger: Change touch HAL comment
    * Line up with dumpling
    
    Change-Id: I8fd2879af91aa9c62eb6e4092985e0c91f9901df

--- device.mk
+++ device.mk
@@ -53,7 +53,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gf_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gf_input.kl \
     $(LOCAL_PATH)/keylayout/synaptics_s3320.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_s3320.kl
 
-# Lineage hardware
+# Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.cheeseburger
 
