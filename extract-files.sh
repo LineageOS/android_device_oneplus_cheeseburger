7e2459275d803172d41c2e90d94795152e2dcc0a
LuK1337 <priv.luk@gmail.com>
2023-10-21 10:07:10 +0200

    cheeseburger: Update libstdc++.vendor target name
    Change-Id: I9fb48d50df7b8b74d19bfb05b3d9c2ca169bf3e2

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libSonyIMX350PdafLibrary.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
