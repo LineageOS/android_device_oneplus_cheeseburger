ec34d431438c7ab021101cc61f804a9784025ab8
LuK1337 <priv.luk@gmail.com>
2025-01-03 15:45:50 +0000

    cheeseburger: Switch to py extract utils
    Change-Id: Ia13b728464495ff208ca1a706847b11af5c93ae8

---
+++ extract-files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+namespace_imports = [
+    'vendor/oneplus/msm8998-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/libSonyIMX350PdafLibrary.so': blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'cheeseburger',
+    'oneplus',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'msm8998-common', module.vendor
+    )
+    utils.run()
